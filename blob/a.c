631a40906c9365f1fca537c97b384b3263912604
halal-beef <78730004+halal-beef@users.noreply.github.com>
2024-01-15 22:12:22 +0000

    multiboot support, kinda useless rightnow due to some issues

---
+++ blob/a.c
@@ -0,0 +1,7 @@
+// Type your code here, or load an example.
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    abort();
+  }
--- main/.main.o.d
+++
@@ -1,4 +0,0 @@
-main.o: main/main.c /usr/aarch64-linux-gnu/include/stdc-predef.h \
- include/generated/autoconf.h include/main.h \
- /usr/lib/gcc/aarch64-linux-gnu/13.2.0/include/stddef.h \
- include/bootmenu/bootmenu.h include/keys.h
