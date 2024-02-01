f8ffb05f8ac3b994010dd26d83589f234d9d36fc
halal-beef <78730004+halal-beef@users.noreply.github.com>
2024-02-01 22:59:47 +0000

    Update debug.c

--- lib/debug/debug.c
+++ lib/debug/debug.c
@@ -35,13 +35,3 @@ void printk(char *text) {
 	debug_linecount++;
 #endif
 }
-
-/* Define our own 128 bit memcpy */
-/*void memcpy(void *dest, void *src, int size)
-{
-	unsigned __int128 *src2 = src;
-	unsigned __int128 *dest2 = dest;
-	for (int i=0; i<size/16; i++)
-		dest2[i] = src2[i];
-}
-*/
